9aec0c9756e17178301b0c33077023039723f92a
betomoedano <betomoedano@outlook.com>
2023-02-21 23:50:22 -0500

    UI improvements

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,6 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Coordinate } from "../types/types";
 
+function getRandomFruitEmoji() {
+  const fruitEmojis = ["🍎", "🍊", "🍋", "🍇", "🍉", "🍓", "🍑", "🍍"];
+  const randomIndex = Math.floor(Math.random() * fruitEmojis.length);
+  return fruitEmojis[randomIndex];
+}
+
 export default function Food({ x, y }: Coordinate): JSX.Element {
   return <Text style={[{ top: y * 10, left: x * 10 }, styles.food]}>🍎</Text>;
 }
